0aecc5a6e23a04c94c96dbe2f934862d9b8b6d60
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-03-29 04:57:03 -0500

    finished the front end design of the application

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -30,6 +30,22 @@ function Home() {
                   </form>
               </div>
           </div>
+          <div className="row rounded shadow-lg m-5 p-3">
+            <div className="col-md-12">
+              <p>
+                <h2>Title:</h2><h5>Lord of The Rings</h5>
+                <hr />
+              </p>
+              <p>
+                <h3>Author:</h3><h5>J. R. R. Tolkien</h5>
+                <hr />
+              </p>
+              <img src="https://upload.wikimedia.org/wikipedia/en/8/8a/The_Lord_of_the_Rings_The_Fellowship_of_the_Ring_%282001%29.jpg" className="img-thumbnail float-left mr-3" alt="Book"></img>
+              <p className="lead">The Lord of the Rings is the saga of a group of sometimes reluctant heroes who set forth to save their world from consummate evil. Its many worlds and creatures were drawn from Tolkien's extensive knowledge of philology and folklore.</p>
+              <button className="btn btn-success" style={{ float: "right" }}>Save</button>
+              <button className="btn btn-success mr-1" style={{ float: "right" }}>View</button>
+            </div>
+          </div>
       </div>
   )
 };
